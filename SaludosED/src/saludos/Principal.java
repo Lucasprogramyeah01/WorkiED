e449b93470f78ee8e39189ae9241c9fb568b1de6
LucasFallaUrtiaga <falla.urluc24_triana.salesianos.edu>
2023-11-21 14:07:36 +0100

    Mejora del saludo a los alumnos.

--- SaludosED/src/saludos/Principal.java
+++ SaludosED/src/saludos/Principal.java
@@ -5,8 +5,19 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		System.out.println("Saludos a todos los alumnos de ED :P");
-	
+		System.out.println("-".repeat(50));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
